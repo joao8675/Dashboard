f3c0d3f03e2ee4358eb33e6fe71baffbbd875fc6
joao8675 <62406945+joao8675@users.noreply.github.com>
2024-01-20 12:23:22 -0300

    timerFunction

---
+++ Scripts/digitalTimerScripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const horas = document.getElementById('horas');
+    const minutos = document.getElementById('minutos');
+
+    const relogio = setInterval(function time() {
+        
+        let dataAtual = new Date();
+        let hr = dataAtual.getHours();
+        let min = dataAtual.getMinutes();
+
+        if (hr < 10) hr = '0' + hr
+
+        if (min < 10) min = '0' + min
+
+        horas.textContent = hr;
+        minutos.textContent = min;
+        segundos.textContent = s;
+
+    }, 1000);
+
+});
