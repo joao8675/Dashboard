5c10c834cce2162edff1358c3f2a005cb19613d9
joao8675 <62406945+joao8675@users.noreply.github.com>
2024-01-24 15:46:19 -0300

    cards

--- Scripts/chartsGraphic.js
+++ Scripts/chartsGraphic.js
@@ -2,27 +2,26 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
-  // Crie dois conjuntos de dados para o gráfico de linha
   var data = {
     labels: ['Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro'],
     datasets: [
       {
         label: 'ETH',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 3,
-        data: Array.from({ length: 8 }, () => getRandomInt(20000, 90000)),
-        fill: false, // Não preencher a área sob a linha
-        cubicInterpolationMode: 'monotone' // Adiciona curvas suaves
+        backgroundColor: '#fff',
+        borderColor: '#ff0000',
+        borderWidth: 2.5,
+        data: Array.from({ length: 8 }, () => getRandomInt(40000, 70000)),
+        fill: false,
+        cubicInterpolationMode: 'monotone'
       },
       {
         label: 'BTC',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        borderWidth: 3,
-        data: Array.from({ length: 8 }, () => getRandomInt(20000, 90000)),
-        fill: false, // Não preencher a área sob a linha
-        cubicInterpolationMode: 'monotone' // Adiciona curvas suaves
+        backgroundColor: '#fff',
+        borderColor: '#001eff',
+        borderWidth: 2.5,
+        data: Array.from({ length: 8 }, () => getRandomInt(20000, 50000)),
+        fill: false,
+        cubicInterpolationMode: 'monotone'
       }
     ]
   };
@@ -51,10 +50,8 @@ function getRandomInt(min, max) {
     }
   };
   
-  // Obtenha o contexto do canvas
   var ctx = document.getElementById('lineChart').getContext('2d');
   
-  // Crie o gráfico de linha
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: data,
