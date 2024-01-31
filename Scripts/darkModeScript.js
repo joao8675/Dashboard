6865e55652f8f1167ab9037fab7d7e778ff9ca42
joao8675 <62406945+joao8675@users.noreply.github.com>
2024-01-31 09:36:44 -0300

    fastPayment

--- Scripts/darkModeScript.js
+++ Scripts/darkModeScript.js
@@ -17,7 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('.linkHover3').classList.toggle('dark');
         document.querySelector('.linkHover4').classList.toggle('dark');
         document.querySelector('.graphic').classList.toggle('dark');
-        document.querySelector('.insideBelowInfos').classList.toggle('dark');
+        document.querySelector('.h1Container').classList.toggle('dark');
+        document.querySelector('.inf1').classList.toggle('dark');
+        document.querySelector('.inf2').classList.toggle('dark');
+        document.querySelector('.inf3').classList.toggle('dark');
+        document.querySelector('.inf4').classList.toggle('dark');
         // Adicione mais classes ou elementos que deseja modificar
     });
 });
